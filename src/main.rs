bf1330933cac10534d1a4066481bd2b6a44cb9d7
Ernest Rudnicki <e.rudnicki96@gmail.com>
2023-06-01 23:06:59 +0200

    feat: create mock users route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ App, HttpServer};
+use users::routes::{get_user};
+
+mod users;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_user)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
